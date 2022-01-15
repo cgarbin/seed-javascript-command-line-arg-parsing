f011acdb94a1a61a860ec8bc8d3a0daf3ff58291
Christian Garbin <garbin.christian@gmail.com>
2022-01-15 15:23:54 -0500

    Update modules to latest versions

--- index.js
+++ index.js
@@ -33,52 +33,63 @@
 "use strict";
 
 const program = require("commander");
+
+// The opearation as separate modules to illustrate modularity and test (npm test)
 const add = require("./lib/add");
 const multiply = require("./lib/multiply");
+
+program.version("0.2.0")
+  .description("Adds or multiplies two numbers")
+
+// Two mandatory arguments: the numbers to add or multiply
+program.arguments("<number1> <number2>")
+
+// An optional operation (defaults to 'add')
+program.option(
+  "-o, --operation <operation>",
+  "Operation to perform (add or multiply)",
+  "add"
+)
+
+// A boolean option (an option without any value)
+program.option("-v, --verbose", "Verbose mode")
 
 let number1 = 0;
 let number2 = 0;
 
-// number1 number1 [--operation add|multiply] (default 'add') [--verbose|-v]
-program
-  .version("0.2.0")
-  .description("Adds or multiplies two numbers")
-  // Two arguments
-  .arguments("<number1> <number2>")
-  // Option with two values and default
-  .option(
-    "-o, --operation <operation>",
-    "Operation to perform (add or multiply)",
-    /^(add|multiply)$/i,
-    "add"
-  )
-  // Option without any value
-  .option("-v, --verbose", "Verbose mode")
-  .action((n1, n2) => {
-    number1 = parseInt(n1, 10);
-    number2 = parseInt(n2, 10);
-    if (isNaN(number1) || isNaN(number2)) {
-      console.error("Please enter valid numbers");
-      process.exit(1);
-    }
-  })
-  .parse(process.argv);
+program.action((n1, n2) => {
+  number1 = parseInt(n1, 10);
+  number2 = parseInt(n2, 10);
+  if (isNaN(number1) || isNaN(number2)) {
+    console.error("Please enter valid numbers");
+    process.exit(1);
+  }
+});
 
 // Show help if no option given (also exits the program)
+// Must be checked before attempting to parse the arguments
 if (program.args.length === 0) {
   program.help();
 }
 
-if (program.verbose) {
+program.parse();
+
+const options = program.opts();
+
+if (options.verbose) {
   // Logs to stderr, so stdout can be redirected without extraneous information
   console.warn("Command line arguments and options:");
-  console.warn("   Number1:   " + number1);
-  console.warn("   Number2:   " + number2);
-  console.warn("   Operation: " + program.operation);
+  console.warn(`   Number1:   ${number1}`);
+  console.warn(`   Number2:   ${number2}`);
+  console.warn(`   Operation: ${options.operation}`);
 }
 
-if (program.operation === "add") {
+if (options.operation === "add") {
   console.log("Number1 + Number2 = " + add.add(number1, number2));
-} else {
+} else if (options.operation === "multiply") {
   console.log("Number1 * Number2 = " + multiply.multiply(number1, number2));
+} else {
+  console.error(`Unknown operation: ${options.operation}`);
+  //  Exit with an error code to indicate failure
+  process.exit(1);
 }
